ce577ed3ff480ba5f18afbd7cf81edfaba485a08
Yurii <Yurii@Admins-MacBook-Pro.local>
2024-12-04 14:46:54 +0200

    ADP | [backend] Create documentation | Clear comments | Remove extra files

--- backend/swagger.js
+++ backend/swagger.js
@@ -1,12 +1,5 @@
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
-// import express from 'express';
-// import path from 'path';
-// import { fileURLToPath } from 'url';
-
-// Get the current file path and directory name
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
 
 const options = {
   definition: {
@@ -24,26 +17,12 @@ const options = {
       },
     ],
   },
-  // looks for configuration in specified directories
   apis: ["./src/routes/*.js"],
 };
 const swaggerSpec = swaggerJsdoc(options);
 function swaggerDocs(app, port) {
-//   // Serve the custom CSS file
-//   app.use(
-//     "/swagger-custom.css",
-//     express.static(path.join(__dirname, "swagger-custom.css"))
-//   );
-
-  // Swagger Page with custom CSS
-//   app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
-//     customCssUrl: '/swagger-custom.css'
-//   }));
-
-  // Swagger Page with custom CSS
   app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-  // Documentation in JSON format
   app.get("/docs.json", (req, res) => {
     res.setHeader("Content-Type", "application/json");
     res.send(swaggerSpec);
